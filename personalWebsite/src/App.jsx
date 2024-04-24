b6a49e296866b762d1e50ef1a5a52fa11d539709
Sefaciftci <sefaciftci.cs@gmail.com>
2024-04-24 12:12:29 +0300

    installation of tailwind

--- personalWebsite/src/App.jsx
+++ personalWebsite/src/App.jsx
@@ -1,12 +1,11 @@
-
+import './index.css';
 
 function App() {
 
-
   return (
-    <div>
-      TEST
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
