205a52b25eb1b46d9e04f58781298c1c5991acb5
Sefaciftci <sefaciftci.cs@gmail.com>
2024-04-25 20:20:41 +0300

    navbar section

--- personalWebsite/src/App.jsx
+++ personalWebsite/src/App.jsx
@@ -1,11 +1,37 @@
 import './index.css';
+import { WiMoonAltThirdQuarter } from "react-icons/wi";
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className='bg-white px-10'>
+      <section>
+        <nav className='flex justify-between py-8 mb-10'>
+          <h1 className='text-2xl'>SefaCiftci</h1>
+          <ul className='flex items-center text-l'>
+            <li><WiMoonAltThirdQuarter className='text-3xl cursor-pointer'/></li>
+            <li className='ml-3 text-xl py-1 px-3 rounded-lg'> <a>Blog</a></li>
+            <li className='ml-3 text-xl py-1 px-3 rounded-lg'>Resume</li>
+          </ul>
+        </nav>
+
+        <div>
+          <h3></h3>
+          <p></p>
+        </div>
+
+        <div>
+          <a></a>
+          <a></a>
+          <a></a>
+        </div>
+
+        <div>
+          <img></img>
+        </div>
+
+      </section>
+    </main>
   )
 }
 
