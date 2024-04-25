3c18f297deeb0af2762bd29c5df130e7eea86ecd
Sefaciftci <sefaciftci.cs@gmail.com>
2024-04-25 20:47:50 +0300

    main section

--- personalWebsite/src/App.jsx
+++ personalWebsite/src/App.jsx
@@ -1,12 +1,17 @@
 import './index.css';
 import { WiMoonAltThirdQuarter } from "react-icons/wi";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
 
 function App() {
 
   return (
     <main className='bg-white px-10'>
+
+      
       <section>
-        <nav className='flex justify-between py-8 mb-10'>
+        <nav className='flex justify-between py-8 mb-12'>
           <h1 className='text-2xl'>SefaCiftci</h1>
           <ul className='flex items-center text-l'>
             <li><WiMoonAltThirdQuarter className='text-3xl cursor-pointer'/></li>
@@ -15,22 +20,24 @@ function App() {
           </ul>
         </nav>
 
-        <div>
-          <h3></h3>
-          <p></p>
+        <div className='text-center'>
+          <h3 className='text-3xl mb-3 tracking-wide	'>SEFA CİFTCİ</h3>
+          <h6 className='text-xl text-gray-500 mb-5' >Software Developer</h6>
+          <p className='text-gray-500'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A iusto, laudantium iure rem numquam et minus quia quisquam necessitatibus nemo?</p>
         </div>
 
-        <div>
-          <a></a>
-          <a></a>
-          <a></a>
+        <div className='flex justify-center text-4xl my-8 gap-10'> 
+          <a className='cursor-pointer'><FaLinkedin /></a>
+          <a className='cursor-pointer'><FaGithub /></a>
+          <a className='cursor-pointer'><SiGmail /></a>
         </div>
-
-        <div>
-          <img></img>
+        
+        <div className='w-60 h-60 flex my-10 mx-auto'>
+          <img className='rounded-full' src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'></img>
         </div>
-
       </section>
+
+
     </main>
   )
 }
