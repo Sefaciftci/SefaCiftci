eac63c96d9c6a845a89a7826e86eb9186f444d5e
Sefaciftci <sefaciftci.cs@gmail.com>
2024-04-27 10:15:50 +0300

    dark mode

--- personalWebsite/src/App.jsx
+++ personalWebsite/src/App.jsx
@@ -3,38 +3,45 @@ import { WiMoonAltThirdQuarter } from "react-icons/wi";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
+import { useState } from "react";
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false)
+
   return (
-    <main className="bg-white px-10 md:px-24">
+    <div className={darkMode? 'dark' : '' }>
+
+    
+    <main className="bg-white px-10 md:px-24 dark:bg-gray-900 min-h-screen">
       <section>
         <nav className="flex justify-end py-10  mb-12 md:mb-16">
           <ul className="flex items-center text-l">
-            <li>
-              <WiMoonAltThirdQuarter className="text-3xl cursor-pointer" />
+            <li onClick={()=> setDarkMode(!darkMode)}>
+              <WiMoonAltThirdQuarter className="text-3xl cursor-pointer dark:bg-white dark:rounded-full" />
             </li>
             
           </ul>
         </nav>
 
-        <div className="md:flex">
-          <div className="text-center py-10 md:w-1/2">
+        <div className="md:flex ">
+          <div className="text-center py-10 md:w-1/2 dark:text-white">
             <h3 className="text-4xl mb-3 tracking-wide">SEFA CİFTCİ</h3>
-            <h6 className="text-2xl text-gray-900 mb-5">Software Developer</h6>
-            <p className="text-gray-700 md:my-8">
+            <h6 className="text-2xl text-gray-900 mb-5 dark:text-white">Software Developer </h6>
+            <p className="text-gray-700 md:my-8 dark:text-white">
               Yeniliklere açık, öğrenmeye istekli ve takım çalışmasına yatkın
               biriyim. Kendimi sürekli geliştirmeye ve yeni bilgiler edinmeye
               çalışıp bireysel gelişimime önem veririm.
             </p>
 
-            <div className="flex justify-center text-4xl my-8 md:my-16 gap-10 text-gray-600">
-              <a className="cursor-pointer hover:text-gray-900 ease-in duration-300">
+            <div className="flex justify-center text-4xl my-8 md:my-16 gap-10 text-gray-600 dark:text-gray-500 ">
+              <a className="cursor-pointer hover:text-gray-900 ease-in duration-300 hover:dark:text-gray-600">
                 <FaLinkedin />
               </a>
-              <a className="cursor-pointer hover:text-gray-900 ease-in duration-300">
+              <a className="cursor-pointer hover:text-gray-900 ease-in duration-300 hover:dark:text-gray-600">
                 <FaGithub />
               </a>
-              <a className="cursor-pointer hover:text-gray-900 ease-in duration-300">
+              <a className="cursor-pointer hover:text-gray-900 ease-in duration-300 hover:dark:text-gray-600">
                 <SiGmail />
               </a>
             </div>
@@ -51,7 +58,7 @@ function App() {
         </div>
 
           <div className="flex justify-center py-6">
-            <a className="py-4 px-10  text-xl rounded-lg cursor-pointer bg-gray-600 text-white hover:bg-gray-900 focus: ease-in duration-300">
+            <a className="py-4 px-10  text-xl rounded-lg cursor-pointer bg-gray-600 text-white hover:bg-gray-900 focus: ease-in duration-300 dark:hover:bg-slate-500">
             Resume
             </a>
           </div>
@@ -59,6 +66,7 @@ function App() {
 
       </section>
     </main>
+    </div>
   );
 }
 export default App;
